5c9c114422300c657b22f8b7c7248c7e7d6f64d3
Chiaki <Chiakiitigodaifuku@gmail.com>
2026-02-18 06:25:01 +0000

    バリデーションの追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,8 @@ class Micropost < ApplicationRecord
     belongs_to :user
 
     # ポストは最大140文字
-    validates :content, length: { maximum: 140}
+    validates :content, length: { maximum: 140},
+
+    # マイクロポストが空欄じゃないかの確認
+                        presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     # ユーザーは複数のマイクロポストがある
     has_many :microposts
+
+    # 空欄だと保存できない
+    validates :name, presence: true
+    validates :email, presence: true
 end
