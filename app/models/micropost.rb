cbcabedf9c90dfb45be639225cf936fcfbc5925c
Chiaki <Chiakiitigodaifuku@gmail.com>
2026-02-18 05:28:28 +0000

    ver1

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ApplicationRecord
+    #マイクロポストは1人のユーザーに属する 
+    belongs_to :user
+
+    # ポストは最大140文字
     validates :content, length: { maximum: 140}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    # ユーザーは複数のマイクロポストがある
+    has_many :microposts
 end
